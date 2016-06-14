45873f57342debbd12780db8b3b61334d6714d10
Gerd Aschemann <gerd@aschemann.net>
2016-06-14 21:48:34 +0200

    Fixed Jenkins

--- modules/jenkins-native/puppet/init.pp
+++ modules/jenkins-native/puppet/init.pp
@@ -39,16 +39,16 @@ $plugins = [
 
 jenkins::plugin { $plugins : }
 
-## This seems to ensure that the initial "unlock" wizard is skipped!
-#file { '/var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion':
-#  owner    => 'jenkins',
-#  group    => 'jenkins',
-#  mode     => 0644,
-#  # TODO: Check how we can dynamically replace this with the current Jenkins version
-#  content  => "2.8",
-#  require  => Class['jenkins'],
-#  replace  => 'no',
-#}
+# This seems to ensure that the initial "unlock" wizard is skipped!
+file { '/var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion':
+  owner    => 'jenkins',
+  group    => 'jenkins',
+  mode     => 0644,
+  # TODO: Check how we can dynamically replace this with the current Jenkins version
+  content  => "2.8",
+  require  => Class['jenkins'],
+  replace  => 'no',
+}
 
 # Add "admin" user with password "admin"
 file { '/var/lib/jenkins/users/admin/config.xml':
